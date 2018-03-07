fd0886d5fbcedf3fdbc1cad417216d807596fbb8
ornelahunda <ornelahunda@gmail.com>
2018-03-07 09:25:10 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,8 +7,8 @@ var questionArray =[
     "Who is chairman, CEO and founder of America Online?",
     "Who is the inventor of the C++ programming language?",
     "Who was the father of Hypertext",
-    "Who created the Mosaic Browser and was co-founder of Netscape Communications?"
-];
+    "Who created the Mosaic Browser and was co-founder of Netscape Communications?"];
+
 
 var answerArray = [
 ["Konrad Zuse", "Charles Babbage", "Alan Turing", "George Boole"],
@@ -22,26 +22,26 @@ var answerArray = [
 
 // Create two variables that are going to hold the correct answers 
 // and the image related to the answer.
-var correctAnswers = ["B.Charles Babbage",
-                      "D.Vint Cerf",
-                      "C.Rasmus Lerdorf",
-                      "C.Steve Case",
-                      "B.Bjarne Stroustrup",
-                      "D.Ted Nelson",
-                      "C.Marc Andreessen",
+var correctAnswer = ["B. Charles Babbage",
+                      "D. Vint Cerf",
+                      "C. Rasmus Lerdorf",
+                      "C. Steve Case",
+                      "B. Bjarne Stroustrup",
+                      "D. Ted Nelson",
+                      "C. Marc Andreessen",
                     ];
-var imageArray = ["<img class='center-block img-right' src='img/babbage.jpeg'>",
-"<img class='center-block img-right' src='cerf.jpeg'>",
-"<img class='center-block img-right' src='rasmus.jpg'>",
-"<img class='center-block img-right' src='case.jpg'>",
-"<img class='center-block img-right' src='bjarne.jpg'>",
-"<img class='center-block img-right' src='ted.jpd'>",
-"<img class='center-block img-right' src='marc.jpg'>"];
+var imageArray = ["<img class='center-block img-right' src='assets/images/babbage.jpeg'>",
+"<img class='center-block img-right' src='assets/images/cerf.jpeg'>",
+"<img class='center-block img-right' src='assets/images/rasmus.jpg'>",
+"<img class='center-block img-right' src='assets/images/case.jpg'>",
+"<img class='center-block img-right' src='assets/images/bjarne.jpg'>",
+"<img class='center-block img-right' src='assets/images/ted.jpd'>",
+"<img class='center-block img-right' src='assets/images/marc.jpg'>"];
 
 
 // This variables will hold the html 
-var startHtml;
-var changeHtml;
+var start;
+var change;
 
 // seconds counter, variable to count questions, variables to count answers, selected answer
 var timeCounter = 30;
@@ -55,15 +55,19 @@ var questionNr=0;
 
 //Starting game function.
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 function startHtml() {
-start= "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+start=$("<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start </a></p>");
+var pTag =$("<p class='text center'>");
+pTag.text("this is a jquery ptag");
+// $(".mainArea").append(pTag);
 	$(".mainArea").html(start);
 }
 startHtml();
 
 //This function will generate the html content after clicking on start button.
+
 
 $("body").on("click", ".start-button", function(event){
   
@@ -77,12 +81,16 @@ $("body").on("click", ".start-button", function(event){
 
 $("body").on("click", ".answer", function(event){
 
+
 	selectedAnswer = $(this).text();
-	if(selectedAnswer === correctAnswers[questionNr]) {
+	console.log(selectedAnswer);
+	console.log(correctAnswer[questionNr]);
+	if(selectedAnswer === correctAnswer[questionNr]) {
 	
 
 		clearInterval(time);
 		win();
+		
 	}
 	else {
 		
@@ -101,18 +109,18 @@ $("body").on("click", ".reset-button", function(event){
 // creating different functions which we need to utilize in our main function above
 
 function reset() {
-     timeCounter = 30;
+    timeCounter = 30;
     correct = 0;
     incorrect = 0;
     unanswered = 0;
-    questionCounter = 0;
+    questionNr = 0;
   changeHtml();
     timer();
 }
 
 // Function timer is needed to set a time for each question and reseting time 
 // accorting to the game functionality.
-function timer() { 
+function timer() {  
    time = setInterval(seconds, 1000);
 	function seconds() {
 		if (timeCounter === 0) {
@@ -129,34 +137,35 @@ function timer() {
 
 function unansweredQuestions() {
 	unanswered++;
-    questionsHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " +correctAnswer[questionNr] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-	$(".mainArea").html(questionsHtml);
+   change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " +correctAnswer[questionNr] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(change);
 	setTimeout(end, 4000); 
 }
 
 function win() {
 	correct++;
-	questionsHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +timeCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswer[questionNr] + "</p>" + imageArray[questionCounter];
-	$(".mainArea").html(questionsHtml);
+	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +timeCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswer[questionNr] + "</p>" + imageArray[questionNr];
+	$(".mainArea").html(change);
 	setTimeout(end, 4000); 
 }
 
 function loss() {
 	incorrect++;
-	questionsHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-	$(".mainArea").html(questionsHtml);
+	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswer[questionNr] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
+	$(".mainArea").html(change);
 	setTimeout(end, 4000); 
 }
 
 function changeHtml() {
-	questionsHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionsArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
-	$(".mainArea").html(questionsHtml);
+	change ="<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionNr] + "</p><p class='first-answer answer'> A. " + answerArray[questionNr][0]+ "</p><p class='answer'>B. "+answerArray[questionNr[1]]+"</p><p class='answer'>C. "+answerArray[questionNr[2]]+"</p><p class='answer'>D. "+answerArray[questionNr[3]]+"</p>";
+	$(".mainArea").html(change);
 }
+
 function end() {
-	if (questionCounter < 7) {
-	questionCounter++;
+	if (questionNr < 7) {
+	questionNr++;
 	changeHtml();
-	counter = 30;
+	timeCounter = 30;
 	timer();
 	}
 	else {
@@ -164,6 +173,6 @@ function end() {
 	}
 }
 function endHtml() {
-	questionsHtml = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-	$(".mainArea").html(questionsHtml);
+	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correct + "</p>" + "<p>Wrong Answers: " + incorrect + "</p>" + "<p>Unanswered: " + unanswered + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	$(".mainArea").html(change);
 }
