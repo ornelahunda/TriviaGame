dc65da597de9542f8a383d83dcac5a909b791bb5
ornelahunda <ornelahunda@gmail.com>
2018-03-08 11:28:45 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,7 @@ var answerArray = [
 ["Konrad Zuse", "Charles Babbage", "Alan Turing", "George Boole"],
 ["Bjarne Stroustrup", "Rasmus Lerdorf", "Sabeer Bhatia", "Vint Cerf"],
 ["Phil Zimmermann","Linus Torvalds", "Rasmus Lerdorf", "Larry Wall"],
-["Larry Ellison", "Jerry Jang", "Steve Case", "jeff Bezos"],
+["Larry Ellison", "Jerry Jang", "Steve Case", "Jeff Bezos"],
 ["Charles Goldfarb","Bjarne Stroustrup","Ray Tomlinson","Phil Zimmermann"],
 ["Larry Wall","Dough Englebart","Linus Torvalds","Ted Nelson"],
 ["Dennis Ritchie", "Paul Alen","Marc Andreessen", "Doug Englebart"]
@@ -61,12 +61,11 @@ function startHtml() {
 start=$("<p class='text-center main-button-container'><a class='btn btn-primary btn-lg start-button' href='#' role='button'>Start </a></p>");
 var pTag =$("<p class='text center'>");
 pTag.text("this is a jquery ptag");
-// $(".mainArea").append(pTag);
 	$(".mainArea").html(start);
 }
 startHtml();
 
-//This function will generate the html content after clicking on start button.
+//This function will generate the html content after clicking on start button, and also starting the timer
 
 
 $("body").on("click", ".start-button", function(event){
@@ -135,31 +134,36 @@ function timer() {
 
 }
 
+
+// When user does not answer
 function unansweredQuestions() {
 	unanswered++;
-   change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " +correctAnswer[questionNr] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+   change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " +correctAnswer[questionNr] + "</p>" + "<img class='center-block img-wrong' src='assets/images/ximage.jpg'>";
 	$(".mainArea").html(change);
-	setTimeout(end, 4000); 
+	setTimeout(end, 3000); 
 }
-
+// When user does guesses correctly
 function win() {
 	correct++;
 	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" +timeCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswer[questionNr] + "</p>" + imageArray[questionNr];
 	$(".mainArea").html(change);
-	setTimeout(end, 4000); 
+	setTimeout(end, 3000); 
 }
-
+//// When user does guess is wrong
 function loss() {
 	incorrect++;
-	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswer[questionNr] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
-	$(".mainArea").html(change);
-	setTimeout(end, 4000); 
+	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswer[questionNr] + "</p>" + "<img class='center-block img-wrong' src='assets/images/ximage.jpg'>";
+	$(".mainArea").html(change); 
+	setTimeout(end, 3000); 
 }
 
+// Html for different questions
 function changeHtml() {
-	change ="<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionNr] + "</p><p class='first-answer answer'> A. " + answerArray[questionNr][0]+ "</p><p class='answer'>B. "+answerArray[questionNr[1]]+"</p><p class='answer'>C. "+answerArray[questionNr[2]]+"</p><p class='answer'>D. "+answerArray[questionNr[3]]+"</p>";
+
+	change ="<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionNr] + "</p><p class='first-answer answer'> A. " + (answerArray[questionNr])[0]+ "</p><p class='answer'>B. "+(answerArray[questionNr])[1]+"</p><p class='answer'>C. "+(answerArray[questionNr])[2]+"</p><p class='answer'>D. "+(answerArray[questionNr])[3]+"</p>";
 	$(".mainArea").html(change);
 }
+
 
 function end() {
 	if (questionNr < 7) {
@@ -172,7 +176,9 @@ function end() {
 		endHtml();
 	}
 }
+
+// displays the results
 function endHtml() {
-	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correct + "</p>" + "<p>Wrong Answers: " + incorrect + "</p>" + "<p>Unanswered: " + unanswered + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	change = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timeCounter + "</span></p>" + "<p class='text-center'>Your results:" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correct + "</p>" + "<p>Wrong Answers: " + incorrect + "</p>" + "<p>Unanswered: " + unanswered + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
 	$(".mainArea").html(change);
 }
